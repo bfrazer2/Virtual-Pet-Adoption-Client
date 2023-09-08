bcffdfde798d01a8506d28a9b23d6b96b819fd75
Benjamin Frazer <105686628+bfrazer2@users.noreply.github.com>
2023-09-07 21:20:42 -0400

    Revert "updated routes for prod"
    This reverts commit ce057e692381a97468a9520275c94d0dea4cee4a.
    
    fixing prod

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 //Auth0 Imports
+import { withAuthenticationRequired } from '@auth0/auth0-react';
 import { useAuth0 } from "@auth0/auth0-react";
 
 //Native Imports
 //Components
 import { DashBoard } from './components/Dashboard/Dashboard';
+import { RedirectToDashboard } from './components/Redirect/redirect';
 import { AccountPage } from './components/AccountPage/AccountPage';
 import { NavBar } from './components/NavBar/NavBar';
 import { CallbackComponent } from './components/CallbackComponent/CallbackComponent';
@@ -34,8 +36,9 @@ export const App = () => {
           <NavBar />
           <Routes>
             <Route path="/dashboard" element={<DashBoard />} />
-            <Route path="/callback" element={<DashBoard />} />
+            <Route path="/callback" element={<CallbackComponent />} />
             <Route path="/account" element={<AccountPage />} />
+            <Route path="/" element={<RedirectToDashboard />} />
           </Routes>
         </Router>
       </PetProvider>
