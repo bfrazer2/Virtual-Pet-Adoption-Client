06975b1fcbbd54876abc571101e5a332c49d7dbb
Benjamin Frazer <105686628+bfrazer2@users.noreply.github.com>
2023-09-07 22:11:22 -0400

    updated base path for prod

--- src/constants.ts
+++ src/constants.ts
@@ -1,2 +1,1 @@
-// export const BASE_PATH = 'https://virtual-pet-adoption-server.onrender.com'
-export const BASE_PATH = 'http://localhost:4000'
+export const BASE_PATH = 'https://virtual-pet-adoption-server.onrender.com'
